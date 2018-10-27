0a6d297113446a4e2941b75c93ecfc011060ba02
ParaschivGeorge <georgegmtt@gmail.com>
2018-10-27 21:15:44 +0300

    make reservation wip

--- src/app/make-reservations/make-reservations.component.ts
+++ src/app/make-reservations/make-reservations.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+import {MaterializeAction} from 'angular2-materialize';
 
 @Component({
   selector: 'app-make-reservations',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakeReservationsComponent implements OnInit {
 
+  reservationForm: FormGroup;
+  date = '';
+  params = [{ format: 'yyyy-mm-dd'}];
+  actions = new EventEmitter<string|MaterializeAction>();
+  roomTypes = ['Video', 'Scrum', 'Enterprise'];
+  roomFloors = [0, 1, 2, 3];
+
   constructor() { }
 
   ngOnInit() {
+    this.reservationForm = new FormGroup({
+      'calendar': new FormControl(null),
+      'roomName': new FormControl(null),
+      'roomTypeForm': new FormControl(['VIDEO']),
+      'roomFloorForm': new FormControl([0]),
+    });
+  }
+
+  dateChanged() {
+    console.log(this.date);
+  }
+
+  onSubmit() {
+    console.log(this.reservationForm);
   }
 
 }
