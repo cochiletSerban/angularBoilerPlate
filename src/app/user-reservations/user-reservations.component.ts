3145d01a63bca7079e8501e3f7c4f38b9971b00a
AdrianStefan23 <adrian.cosmin23@gmail.com>
2018-10-27 23:22:52 +0300

    See reservation component done

--- src/app/user-reservations/user-reservations.component.ts
+++ src/app/user-reservations/user-reservations.component.ts
@@ -14,15 +14,140 @@ export class UserReservationsComponent implements OnInit {
 
   date : '';
   reservations: Reservation[];
+  workingHours = [
+    {
+      id: 1,
+      hour:"08:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+
+    },
+    {
+      id: 2,
+      hour:"09:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 3,
+      hour:"10:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 4,
+      hour:"11:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 5,
+      hour:"12:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 6,
+      hour:"13:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 7,
+      hour:"14:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 8,
+      hour:"15:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 9,
+      hour:"16:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 10,
+      hour:"17:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 11,
+      hour:"18:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 12,
+      hour:"19:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    },
+    {
+      id: 13,
+      hour:"20:00",
+      reserved: false,
+      room: "",
+      floor: "",
+      roomType: ""
+    }
+  ];
+
   constructor (private _getReservationService : GetReservationsService) {}
 
   ngOnInit(){
+  }
+
+  isRoomReserved() {
+    if (this.reservations) {
+      for (let reservation of this.reservations) {
+        let start = reservation.startTime;
+        let end = reservation.endTime;
+        for (let i = start; i < end; i++) {
+          this.workingHours[i-1].reserved = true;
+          this.workingHours[i-1].room = reservation.roomData.name;
+          this.workingHours[i-1].floor = reservation.roomData.floor.toString();
+          this.workingHours[i-1].roomType = reservation.roomData.type;
+        }
+      }
+    }
+    console.log(this.workingHours);
   }
 
   dateChange() {
     this._getReservationService.getReservations(this.date, null, null, null, null, null, null, null).subscribe(resp => {
       console.log(resp);
       this.reservations = resp._embedded.reservation;
+      this.isRoomReserved();
     })
   }
 }
